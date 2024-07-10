7632a20a45384576e495a5536d4ae991def759c6
lv-2323 <dlc.q235@gmail.com>
2024-07-10 17:44:57 -0400

    Actualizar archivo_prueba01.py

--- archivo_prueba01.py
+++ archivo_prueba01.py
@@ -1,1 +1,16 @@
 print('hola mundo')
+import random as rd
+
+class Dado:
+  def __init__(self,color):
+    self.color=color
+    self.valor=0
+  def tirar(self):
+    rdm=rd.randint(1,6)
+    self.set_dado(rdm)
+  def set_dado(self,nuValor):
+    self.valor=nuValor:
+  def get_dado(self):
+    return self.valor
+  def ver_resul(self):
+    print(self.get_dado())
